ccb506716f2edc139f357d4f695e70f72cb83a8a
Luis Aguirre <luisaguirre_19@yahoo.es>
2023-06-08 01:34:35 -0600

    nuevo estado de cuenta

--- src/app/componentes/estado-cuenta/estado-cuenta.component.ts
+++ src/app/componentes/estado-cuenta/estado-cuenta.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { AuthService } from 'src/app/servicios/auth.service';
 import { SqlService } from 'src/app/servicios/sql.service';
-
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
 @Component({
   selector: 'app-estado-cuenta',
   templateUrl: './estado-cuenta.component.html',
@@ -15,7 +16,8 @@ export class EstadoCuentaComponent {
   etiqueta
 
   tableData
-  displayedColumns: string[] = ['id_cuenta', 'Etiqueta', 'Peso', 'Parcialidades', 'Estado'];
+  displayedColumns: string[] = ['id_estado_cuenta', 'id_cuenta', 'peso_esperado', 'peso_total', 'diferencia', 'estado', 'fecha', 'imprimir'];
+  						
   constructor(
     private sqlService:SqlService,
     private authService:AuthService
@@ -27,8 +29,81 @@ export class EstadoCuentaComponent {
 
 
   traer_datos(){
-    this.sqlService.getDataProductor("cuenta").subscribe(resp=>{
-     this.tableData = resp
+    this.sqlService.postData_productor("estado_cuenta",{
+      "correo":'beneficio'
+   })
+   .subscribe(data=>{
+      this.tableData = data
    })
  }
+
+
+ print(id_cuenta,peso_esperado,peso_total,diferencia, estado, fecha) {
+
+  var docDefinition = {
+   // watermark: { text: 'PESO CABAL, S.A.', color: 'blue', opacity: 0.3, bold: true, italics: false },
+    content: [
+      { text: 'Beneficio cafetito S.A.', style: 'subheader' },
+      'A continuación te adjuntamos los datos de cuenta:',
+      {
+        style: 'tableExample',
+        color: '#444',
+        table: {
+          widths: [200, 'auto', 'auto'],
+          headerRows: 2,
+          // keepWithHeaderRows: 1,
+          body: [
+            [{ text: 'Identificador de la cuenta', style: 'tableHeader', colSpan: 2, alignment: 'Left' }, {}, { text: id_cuenta.toString(), style: 'tableHeader', alignment: 'Right' }],
+            [{ text: 'Peso esperado:', style: 'tableHeader', colSpan: 2, alignment: 'Left' }, {}, { text: peso_esperado.toString(), style: 'tableHeader', alignment: 'Right' }],
+            [{ text: 'Peso total', style: 'tableHeader', colSpan: 2, alignment: 'Left' }, {}, { text: peso_total.toString(), style: 'tableHeader', alignment: 'Right' }],
+            [{ text: 'Diferencia', style: 'tableHeader', colSpan: 2, alignment: 'Left' }, {}, { text: diferencia.toString(), style: 'tableHeader', alignment: 'Right' }],
+            [{ text: 'Estado', style: 'tableHeader', colSpan: 2, alignment: 'Left' }, {}, { text: estado.toString(), style: 'tableHeader', alignment: 'Right' }],
+            [{ text: 'Fecha', style: 'tableHeader', colSpan: 2, alignment: 'Left' }, {}, { text: fecha.toString(), style: 'tableHeader', alignment: 'Right' }]
+          ]
+        }
+      }
+    ],
+    styles: {
+      header: {
+        fontSize: 18,
+        bold: true,
+        margin: [0, 0, 0, 10]
+      },
+      subheader: {
+        fontSize: 16,
+        bold: true,
+        margin: [0, 10, 0, 5]
+      },
+      tableExample: {
+        margin: [0, 5, 0, 15]
+      },
+      tableOpacityExample: {
+        margin: [0, 5, 0, 15],
+        fillColor: 'blue',
+        fillOpacity: 0.3
+      },
+      tableHeader: {
+        bold: true,
+        fontSize: 13,
+        color: 'black'
+      }
+    },
+    defaultStyle: {
+      // alignment: 'justify'
+    },
+    patterns: {
+      stripe45d: {
+        boundingBox: [1, 1, 4, 4],
+        xStep: 3,
+        yStep: 3,
+        pattern: '1 w 0 1 m 4 5 l s 2 0 m 5 3 l s'
+      }
+    }
+  };
+  
+  pdfMake.vfs = pdfFonts.pdfMake.vfs;
+
+  const pdfDocGenerator = pdfMake.createPdf(docDefinition);
+  pdfDocGenerator.download('documento.pdf');  // Nombre del archivo de salida
+}
 }
